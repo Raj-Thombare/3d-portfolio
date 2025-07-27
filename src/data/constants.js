192c89ffff0b8c47609659bb73d577a4f2a022bc
Raj Thombare <rajthombare.dev@gmail.com>
2025-07-27 14:34:58 +0530

    push code

---
+++ src/data/constants.js
@@ -0,0 +1,364 @@
+export const Bio = {
+  name: "Raj Thombare",
+  roles: ["Full Stack Developer", "Frontend Developer", "Software Developer"],
+  description:
+    "Full-stack engineer shaping intuitive digital experiences. I architect scalable systems and craft seamless UIs using modern web technologies.",
+  github: "https://github.com/raj-thombare",
+  resume:
+    "https://drive.google.com/file/d/1oxiO7hhlYv157utbtt7wansbShUyGjN6/view",
+  linkedin: "https://www.linkedin.com/in/rajthombare/",
+  twitter: "https://twitter.com/rajth0mbare",
+  insta: "https://www.instagram.com/rajth0mbare/",
+};
+
+export const skills = [
+  {
+    title: "Languages & Tools",
+    skills: [
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+      },
+      {
+        name: "TypeScript",
+        image: "https://cdn.worldvectorlogo.com/logos/typescript.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "SQL",
+        image: "https://cdn-icons-png.flaticon.com/512/4248/4248443.png",
+      },
+    ],
+  },
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "NextJS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg",
+      },
+      {
+        name: "ReactJS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+      },
+      {
+        name: "React Native",
+        image: "https://reactnative.dev/img/header_logo.svg",
+      },
+      {
+        name: "Zustand",
+        image: "https://avatars.githubusercontent.com/u/72518640?s=280&v=4",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png",
+      },
+      {
+        name: "Shadcn/UI",
+        image: "https://avatars.githubusercontent.com/u/139895814?s=200&v=4",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
+      },
+      {
+        name: "HTML",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Backend & DevOps",
+    skills: [
+      {
+        name: "NodeJS",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "ExpressJS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/6/64/Expressjs.png",
+      },
+      {
+        name: "WebSocket",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d9/Icon-round-Question_mark.jpg",
+      },
+      {
+        name: "Redis",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/6/6b/Redis_Logo.svg",
+      },
+      {
+        name: "Kafka",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/05/Apache_kafka_wordtype.svg",
+      },
+      {
+        name: "PostgreSQL",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "Prisma",
+        image: "https://www.prisma.io/images/og-image.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "MongoDB",
+        image: "https://www.mongodb.com/assets/images/global/leaf.png",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+      {
+        name: "AWS (EC2, S3, EBS, RDS, Cloudfront)",
+        image:
+          "https://a0.awsstatic.com/libra-css/images/logos/aws_logo_smile_1200x630.png",
+      },
+      {
+        name: "Docker",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+      },
+      {
+        name: "CI/CD",
+        image: "https://cdn-icons-png.flaticon.com/512/9775/9775352.png", // generic CI/CD icon
+      },
+      {
+        name: "Nginx",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/c/c5/Nginx_logo.svg",
+      },
+      {
+        name: "Cloudflare",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/4/4e/Cloudflare_Logo.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 1,
+    img: "/100xdevs_logo.jpeg",
+    role: "Full Stack Developer (Trainee)",
+    company: "100xDevs",
+    date: "Nov 2023 - Jul 2024",
+    desc: "Built and deployed scalable full-stack applications using NextJS, NodeJS, Express, PostgreSQL, Redis, and WebSockets. Worked with Kafka, Docker, and AWS for high-performance microservices. Integrated CI/CD pipelines and containerized services, with a strong focus on performance optimization, clean code, and debugging best practices.",
+    skills: [
+      "NextJS",
+      "ReactJS",
+      "NodeJS",
+      "Express",
+      "PostgreSQL",
+      "Redis",
+      "Kafka",
+      "Docker",
+      "AWS",
+      "CI/CD",
+      "TypeScript",
+    ],
+    doc: "https://100xdevs.com/",
+  },
+  {
+    id: 2,
+    img: "/shiksha_sopan_logo.jpeg",
+    role: "Frontend Developer Intern",
+    company: "Shiksha Sopan – IIT Kanpur",
+    date: "Apr 2023 - May 2023",
+    desc: "Developed the UI for the NAEST physics competition platform by translating Figma designs into responsive React components. Integrated React Query for API communication, implemented lazy loading and code splitting to improve performance by 40%, and built end-to-end authentication UIs for seamless login and registration flows.",
+    skills: [
+      "ReactJS",
+      "React Query",
+      "TypeScript",
+      "Tailwind CSS",
+      "HTML",
+      "CSS",
+      "Figma",
+      "JavaScript",
+    ],
+    doc: "https://naest.shiksha-sopan.org/",
+  },
+];
+
+export const education = [
+  {
+    id: 1,
+    img: "/amity.png",
+    school: "Amity University Mumbai",
+    date: "Aug 2023 – Jun 2025",
+    grade: "8.32 CGPA",
+    desc: "I am currently pursuing a Master’s degree in Computer Applications at Amity University, Mumbai. During my academic journey, I’ve focused on full-stack web development, cloud technologies, and system design. I've also worked on several industry-level projects and internships alongside my coursework, applying theoretical knowledge to real-world scenarios.",
+    degree: "Master of Computer Applications - MCA",
+  },
+  {
+    id: 2,
+    img: "/csmu.png",
+    school: "Chhatrapati Shivaji Maharaj University",
+    date: "Aug 2019 – Jun 2022",
+    grade: "8.5 CGPA",
+    desc: "Did Bachelor’s degree in Computer Applications at CSMU. During my academic journey, I’ve focused on full-stack web development, cloud technologies, and system design. I've also worked on several industry-level projects and internships alongside my coursework, applying theoretical knowledge to real-world scenarios.",
+    degree: "Bachelor of Computer Applications - BCA",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Quick Chat",
+    date: "Feb 2024 - Mar 2024",
+    description:
+      "Built a scalable real-time chat application that allows users to create and join rooms for group messaging. Integrated Redis Streams with Socket.IO to enable distributed communication across multiple servers and optimized message processing with Kafka. Emphasized fault tolerance and horizontal scalability to handle peak loads.",
+    image: "/projects/quick-chat.png",
+    tags: [
+      "NextJS",
+      "Redis",
+      "Kafka",
+      "Socket.IO",
+      "PostgreSQL",
+      "Express",
+      "TypeScript",
+    ],
+    category: "full stack",
+    github: "https://github.com/raj-thombare/quick-chat",
+    webapp: "https://quickchat.rajthombare.xyz/",
+    member: [
+      {
+        name: "Raj Thombare",
+        img: "https://avatars.githubusercontent.com/u/86057670?v=4",
+        linkedin: "https://linkedin.com/in/rajthombare",
+        github: "https://github.com/raj-thombare",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Clash",
+    date: "Jan 2024 - Feb 2024",
+    description:
+      "Developed a dynamic voting platform for image-based battles with real-time vote updates using Socket.IO. Implemented Redis-based BullMQ queues for background processing, including email notifications and vote syncing. Enabled scalable infrastructure and seamless user interaction with PostgreSQL and WebSocket integration.",
+    image: "/projects/clash.png",
+    tags: [
+      "NextJS",
+      "Socket.IO",
+      "Redis",
+      "BullMQ",
+      "PostgreSQL",
+      "TypeScript",
+      "NodeJS",
+    ],
+    category: "full stack",
+    github: "https://github.com/raj-thombare/clash",
+    webapp: "https://clash.rajthombare.xyz/",
+    member: [
+      {
+        name: "Raj Thombare",
+        img: "https://avatars.githubusercontent.com/u/86057670?v=4",
+        linkedin: "https://linkedin.com/in/rajthombare",
+        github: "https://github.com/raj-thombare",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "ProdStation",
+    date: "Mar 2024 - Apr 2024",
+    description:
+      "Built a modern e-commerce platform with secure authentication, dynamic cart and checkout, and Razorpay integration. Implemented ClerkAuth for secure user management and used Drizzle ORM with PostgreSQL to ensure consistent data operations. Optimized UI for responsiveness and seamless shopping experience.",
+    image: "/projects/prodstation.png",
+    tags: [
+      "NextJS",
+      "TypeScript",
+      "ClerkAuth",
+      "Drizzle ORM",
+      "PostgreSQL",
+      "Razorpay",
+    ],
+    category: "full stack",
+    github: "https://github.com/raj-thombare/prodstation",
+    webapp: "https://prodstation.rajthombare.xyz/",
+    member: [
+      {
+        name: "Raj Thombare",
+        img: "https://avatars.githubusercontent.com/u/86057670?v=4",
+        linkedin: "https://linkedin.com/in/rajthombare",
+        github: "https://github.com/raj-thombare",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "X",
+    date: "Mar 2024 - Apr 2024",
+    description:
+      "Built a social media application just like X. Optimized UI for responsiveness and seamless shopping experience.",
+    image: "/projects/x.png",
+    tags: [
+      "NextJS",
+      "TypeScript",
+      "ClerkAuth",
+      "Drizzle ORM",
+      "PostgreSQL",
+      "Razorpay",
+    ],
+    category: "frontend",
+    github: "https://github.com/raj-thombare/x",
+    webapp: "https://x.rajthombare.xyz/",
+    member: [
+      {
+        name: "Raj Thombare",
+        img: "https://avatars.githubusercontent.com/u/86057670?v=4",
+        linkedin: "https://linkedin.com/in/rajthombare",
+        github: "https://github.com/raj-thombare",
+      },
+    ],
+  },
+  {
+    id: 4,
+    title: "Task AI",
+    date: "Mar 2024 - Apr 2024",
+    description:
+      "A modern saas website landing page for todays developers. Optimized UI for responsiveness and seamless shopping experience.",
+    image: "/projects/task-ai.png",
+    tags: [
+      "NextJS",
+      "TypeScript",
+      "ClerkAuth",
+      "Drizzle ORM",
+      "PostgreSQL",
+      "Razorpay",
+    ],
+    category: "frontend",
+    github: "https://github.com/raj-thombare/task-ai",
+    webapp: "https://x.rajthombare.xyz/",
+    member: [
+      {
+        name: "Raj Thombare",
+        img: "https://avatars.githubusercontent.com/u/86057670?v=4",
+        linkedin: "https://linkedin.com/in/rajthombare",
+        github: "https://github.com/raj-thombare",
+      },
+    ],
+  },
+];
